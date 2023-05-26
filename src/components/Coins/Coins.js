e13f42e783039f3d73ba683322e2296e81fdf611
Sergey <NadeevSA@mail.ru>
2023-05-26 18:36:38 +0500

    26.05

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,37 +1,42 @@
 import './Coins.css'
 import Coin from './Coin'
 import { fetchChart } from '../../api/Requests.js'
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react';
 
 export default function Coins() {
   var [coins, setCoins] = useState(null)
 
-  setInterval(() => {
-    var blockCoins = document.getElementById("blockCoins")
-    if (blockCoins.scrollLeft == blockCoins.scrollWidth - blockCoins.clientWidth) {
-      blockCoins.scrollLeft = 0
-    }
-    blockCoins.scrollLeft += 1
-  }, 50)
-
   useEffect(() => {
     fetchChart().then(res => {
-          setCoins(res)
-      })
+      setCoins(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    const int2 = setInterval(() => {
+      var blockCoins = document.getElementById("blockCoins")
+      if (blockCoins.scrollLeft >= blockCoins.scrollWidth - blockCoins.clientWidth) {
+        blockCoins.scrollLeft = 0
+      }
+      blockCoins.scrollLeft += 1
+    }, 25)
+    return () => {
+      clearInterval(int2)
+    }
   }, [])
 
   return (
     <div id='blockCoins' className="category">
-          {
-            coins ? coins.data.map((element, key) => {
-              return <Coin
-                key={key}
-                explorer = {element.explorer} 
-                name = {element.name}
-                priceUsd = {element.priceUsd}
-                changePercent24Hr = {element.changePercent24Hr} />
-            }) : "Загрузка"
-          }
+      {
+        coins ? coins.data.map((element, key) => {
+          return <Coin
+            key={key}
+            explorer={element.explorer}
+            name={element.name}
+            priceUsd={element.priceUsd}
+            changePercent24Hr={element.changePercent24Hr} />
+        }) : "Загрузка"
+      }
     </div>
   );
 }
